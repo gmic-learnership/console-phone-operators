2c5c5664651285a880b716eb21ea927d85e6edb6
Vivian.Tebeila <VTebeila@DVT-MVTEBEILA.jhbdvt.co.za>
2015-10-22 10:25:42 +0200

    updated rogram

--- PhoneApplication/PhoneApplication/Program.cs
+++ PhoneApplication/PhoneApplication/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PhoneApplication
@@ -11,94 +12,77 @@ namespace PhoneApplication
         static void Main(string[] args)
         {
 
+
+            //declaring variables
+            string tel = "";
+            Boolean isValid;
+            Regex regex = new Regex("^[0-9() -]+$");
+            tel = tel.Replace("(", "");
+
             Console.ForegroundColor = ConsoleColor.Cyan;
-            string tel = Console.ReadLine();
-            while (tel != "x")
-            { 
-            Console.WriteLine("\n\n\t**********Welcome To The Oparetors Phone Dailer***********\n\n");    
-            Console.WriteLine("Please enter your Number Below or Type 'Exit' to exit this program");
-            Console.ForegroundColor = ConsoleColor.White;
-             tel = Console.ReadLine();
+            while (tel != "!")
+            {
+                Console.Write("Enter your number \n or press ! to exit \n");
+                tel = Console.ReadLine();
+                if (regex.IsMatch(tel))
+                {
+                    isValid = true;
+                    if (tel.Contains("(") || tel.Contains(")") || tel.Contains("-") || tel.Contains(" ") || tel.Substring(0, 3) == "012" || tel.Substring(0, 3) == "011" || tel.Substring(0, 3) == "083" || tel.Substring(0, 3) == "072" || tel.Substring(0, 3) == "069" || tel.Substring(0, 3) == "073")
+                    { //first IF
+                        tel = tel.Replace("(", "");
+                        tel = tel.Replace(")", "");
+                        tel = tel.Replace("-", "");
+                        tel = tel.Replace(" ", "");
 
-            if (tel.Contains("(") || tel.Contains(")") || tel.Contains("-") || tel.Contains(" ") || tel.Substring(0, 3) == "012" || tel.Substring(0, 3) == "011" || tel.Substring(0, 3) == "083" || tel.Substring(0, 3) == "072" || tel.Substring(0, 3) == "069" || tel.Substring(0, 3) == "073")
-            { //first IF
-                tel = tel.Replace("(", "");
-                tel = tel.Replace(")", "");
-                tel = tel.Replace("-", "");
-                tel = tel.Replace(" ", "");
-                //Console.WriteLine("successfully Dialling" + " " + tel);
-
-                tel.Trim();
-                double Num;
-                bool isNum = double.TryParse(tel, out Num);
-
-                if (isNum)
-                {// second IF
-                    if (tel.Length == 10)
-                    { //third IF
-                        Console.WriteLine("Successfully Dialling " + tel);
-
-
-
-                        //this a numbers that are saved in the program
-                        if (tel == "0831976923")
+                        if (tel.Length == 10)
                         {
-                            Console.WriteLine(" Calling vivian ");
+                            Console.WriteLine("you dailed " + tel);
+                            switch (tel)
+                            {
+                                case "0831976923":
+                                    Console.WriteLine(" Calling vivian ");
+                                    break;
+                                case "0723456789":
+
+                                    Console.WriteLine("calling Bonginkosi");
+                                    break;
+
+
+                                case "0699876543":
+                                    Console.WriteLine("calling Dantani");
+                                    break;
+
+                                case "0112345678":
+                                    Console.WriteLine("DVT campany");
+                                    break;
+
+                                case "0123692581":
+
+                                    Console.WriteLine("DTH company ");
+                                    break;
+                            }
+
                         }
-                        else if (tel == "0723456789")
+                        else
                         {
-                            Console.WriteLine("Calling Bonginkosi");
+                            Console.WriteLine("your number must have only 10 digits!");
                         }
 
 
-                        else if (tel == "0699876543")
-                        {
-                            Console.WriteLine("Calling Dantani");
-                        }
 
-                        else if (tel == "0112345678")
-                        {
-                            Console.WriteLine("DVT campany");
-                        }
-
-                        else if (tel == "0123692581")
-                        {
-                            Console.WriteLine("DTH company ");
-                        }
-                        else if (tel == "0837443209")
-                        {
-                            Console.WriteLine("Calling Tendani");
-                        }
-                         else if (tel == "Exit")
-                    {
-                        Environment.Exit(0);
-                        Console.ReadLine();
-                    }
                     }
                     else
-                    { // third else(IF)
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Please Check the number and try again");
-                        Console.Beep();
-                    }// third ELSE (IF)
-
-
-
-
-                }// Second aIF
-
+                    {
+                        Console.WriteLine("Your phone number must start only with 012, 011, 069 or 073");
+                    }
+                }
                 else
-                { // second ELSE (if)
-                    Console.WriteLine("tel can only contain numeric values");
-                }//Second ELSE
-            } // first IF
+                {
+                    Console.WriteLine("Invalid");
+                }
 
-
-
-
-            Console.ReadLine();
-        }
-
+                Console.ReadLine();
+            }
 
         }
     }
